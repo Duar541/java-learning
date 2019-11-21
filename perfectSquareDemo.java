21997e2cebe49c417648582b228423bacf229bd3
Duar541 <46703236+Duar541@users.noreply.github.com>
2019-11-21 23:08:54 +0800

    1121第11章第2题

---
+++ perfectSquareDemo.java
@@ -0,0 +1,10 @@
+package aa1107;
+
+public class perfectSquareDemo {
+	 public static void main(String[] args) {
+	perfectSquare a = new perfectSquare();
+	a.getfactor();
+	a.getResult();
+	
+	 }
+}
